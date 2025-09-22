31ca96361a0f303d1bd104e30b270a684e1aaf3a
MUKILTEO\1726409 <rian.francis@studentssd.org>
2025-09-22 11:52:41 -0700

    yedterdat

---
+++ 2D Template/Assets/triggerbox.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class triggerbox : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision");
+        
+    }
+
+
+
+
+
+
+
+
+}
+
+
