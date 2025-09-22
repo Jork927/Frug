1f054c06e0dcd7152c8d8afd7ad57dbf2f5f4082
MUKILTEO\1726409 <rian.francis@studentssd.org>
2025-09-22 13:30:36 -0700

    updated trigger script

--- 2D Template/Assets/triggerbox.cs
+++ 2D Template/Assets/triggerbox.cs
@@ -1,16 +1,20 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class triggerbox : MonoBehaviour
 {
-
+    VariableDeclaration points;
+    public static int player_points = 0;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("collision");
-        
+        Destroy(gameObject);
+        player_points += 1;
+        Debug.Log(player_points);
+
     }
 
-
-
+   
 
 
 
